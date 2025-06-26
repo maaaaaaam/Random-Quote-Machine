d9a643e37c7dd18f4011db5f97042e2365f70c96
maaaaaaam <209742938+maaaaaaam@users.noreply.github.com>
2025-06-26 18:11:54 +0300

    Deleted useless tests

--- frontend/tests/function.test.js
+++
@@ -1,5 +0,0 @@
-import { sum } from './function.js'
-
-describe('sum', () => {
-    it('should add the numbers', () => {expect(sum(1,2)).toBe(3)})
-})
--- frontend/tests/rqm.test.js
+++
@@ -1,5 +0,0 @@
-import { getQuote } from '../src/functions.js'
-
-describe('getQuote', () => {
-    it('should return an object with the text and the author of a quote', () => {expect(getQuote()).toBeTruthy()})
-})
